fb6b8479a7bb3224125c7f60d1775c426b04fbc6
lucenguyen <83016168+lucenguyen@users.noreply.github.com>
2024-05-10 18:12:32 +0700

    add function js swiper for 3 sections

--- js/script.js
+++ js/script.js
@@ -48,6 +48,7 @@ var cardSlider = new Swiper(".card-slider", {
         prevEl: ".swiper-button-prev",
     },
     slidesPerView: 3,
+    // direction: getDirection(),
     spaceBetween: 70,
     breakpoints: {
         // when window is <= 767px
@@ -60,7 +61,18 @@ var cardSlider = new Swiper(".card-slider", {
         spaceBetween: 40,
         },
     },
+    // on: {
+    //     resize: function () {
+    //         cardSlider.changeDirection(getDirection());
+    //     },
+    // },
 });
+// function getDirection() {
+//     var windowWidth = window.innerWidth;
+//     var direction = window.innerWidth <= 767 ? 'vertical' : 'horizontal';
+//
+//     return direction;
+// }
 
 var cardSlider1 = new Swiper(".swiper-join-member", {
     direction: 'vertical',
@@ -134,10 +146,29 @@ let listSuggest = [
     {line1: "Bắt đầu hoạt động từ: 24/02/2021", line2: "Chủ trương hội: Hoạt động năng nổ", line3: "Số lượng thành viên: 15", line4: "Mức độ đánh giá: Cực tốt", img: "assets/images/bao-tram-small.png", name: "Lê Nguyễn Bảo Trâm", role: "Chủ hụi"},
 ];
 
+let listJoinMember = [
+    {img: "assets/images/avatar-member.png", name: "Hoàng Nguyễn Văn Nhân", role: "Thành viên hội"},
+    {img: "assets/images/avatar-member.png", name: "Hoàng Nguyễn Văn Nhân", role: "Thành viên hội"},
+    {img: "assets/images/avatar-member.png", name: "Hoàng Nguyễn Văn Nhân", role: "Thành viên hội"},
+    {img: "assets/images/avatar-member.png", name: "Hoàng Nguyễn Văn Nhân", role: "Thành viên hội"},
+    {img: "assets/images/avatar-member.png", name: "Hoàng Nguyễn Văn Nhân", role: "Thành viên hội"},
+];
+
+let listComment = [
+    {line1: "Purus maecenas quis elit eu, aliquet. Tellus porttitor ut sollicitudin sit non fringilla. Quam nunc volutpat senectus neque eget amet pharetra, euismod. Tempus, nunc, molestie imperdiet curabitur commodo euismod.", name: "Lê Thị Mỹ Hoa", role: "Thành viên hội", img: "assets/images/bao-tram-small.png"},
+    {line1: "Purus maecenas quis elit eu, aliquet. Tellus porttitor ut sollicitudin sit non fringilla. Quam nunc volutpat senectus neque eget amet pharetra, euismod. Tempus, nunc, molestie imperdiet curabitur commodo euismod.", name: "Lê Thị Mỹ Hoa", role: "Thành viên hội", img: "assets/images/bao-tram-small.png"},
+    {line1: "Purus maecenas quis elit eu, aliquet. Tellus porttitor ut sollicitudin sit non fringilla. Quam nunc volutpat senectus neque eget amet pharetra, euismod. Tempus, nunc, molestie imperdiet curabitur commodo euismod.", name: "Lê Thị Mỹ Hoa", role: "Thành viên hội", img: "assets/images/bao-tram-small.png"},
+    {line1: "Purus maecenas quis elit eu, aliquet. Tellus porttitor ut sollicitudin sit non fringilla. Quam nunc volutpat senectus neque eget amet pharetra, euismod. Tempus, nunc, molestie imperdiet curabitur commodo euismod.", name: "Lê Thị Mỹ Hoa", role: "Thành viên hội", img: "assets/images/bao-tram-small.png"},
+    {line1: "Purus maecenas quis elit eu, aliquet. Tellus porttitor ut sollicitudin sit non fringilla. Quam nunc volutpat senectus neque eget amet pharetra, euismod. Tempus, nunc, molestie imperdiet curabitur commodo euismod.", name: "Lê Thị Mỹ Hoa", role: "Thành viên hội", img: "assets/images/bao-tram-small.png"},
+    {line1: "Purus maecenas quis elit eu, aliquet. Tellus porttitor ut sollicitudin sit non fringilla. Quam nunc volutpat senectus neque eget amet pharetra, euismod. Tempus, nunc, molestie imperdiet curabitur commodo euismod.", name: "Lê Thị Mỹ Hoa", role: "Thành viên hội", img: "assets/images/bao-tram-small.png"},
+]
+
 
 
 getListOwners(listOfOwners1);
-// getListSuggest(listSuggest);
+getListSuggest(listSuggest);
+getListJoiMember(listJoinMember);
+getListComment(listComment);
 
 function getListOwners() {
     var list = document.getElementById('owners')
@@ -146,8 +177,11 @@ function getListOwners() {
         var div = `<div class="col-xxl-3 col-md-6 col-sm-12"><div class="member-chuhui justify-content-lg-start">
                             <img  class="img-chu-hui" src="${item.image1}">
                             <div class="d-flex">
+                            <div style="border-radius: 25%">
                                 <img class="m-2" src="${item.avatar}">
-                                <div>
+                            </div>
+
+                                <div >
                                     <p id="name" class="m-2 text-bold">${item.name1}</p>
                                     <p id="role" class="m-2 roles">${item.role1}</p>
                                 </div>
@@ -157,33 +191,145 @@ function getListOwners() {
     }
 }
 
-// function getListSuggest() {
-//     var list = document.getElementById('suggest')
-//
-//     for (let item of listSuggest) {
-//         var div = `<div class="swiper-slide">
-//                             <div class="container testimonial-card pb-5 ">
-//                                 <div  class="member d-flex pt-4 ">
-//                                     <div class="justify-content-lg-start">
-//                                         <div class="m-3" >
-//                                             <p>${item.line1}</p>
-//                                             <p>${item.line2}</p>
-//                                             <p>${item.line3}</p>
-//                                             <p class="mb-3">${item.line4}</p>
-//                                         </div>
-//
-//                                         <div class="d-flex">
-//                                             <img class="m-2" src="${item.img}">
-//                                             <div>
-//                                                 <p class="m-2 text-bold">${item.name}</p>
-//                                                 <p class="m-2">${item.role}</p>
-//                                             </div>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                         </div> <!-- end of swiper-slide -->`
-//         list.innerHTML += div
-//     }
-// }
+function getListSuggest() {
+    var list = document.getElementById('suggest')
 
+    for (let item of listSuggest) {
+        var div = `<div class="swiper-slide">
+                            <div class="container testimonial-card pb-5 ">
+                                <div  class="member d-flex pt-4 ">
+                                    <div class="justify-content-lg-start">
+                                        <div class="m-3" >
+                                            <p>${item.line1}</p>
+                                            <p>${item.line2}</p>
+                                            <p>${item.line3}</p>
+                                            <p class="mb-3">${item.line4}</p>
+                                        </div>
+
+                                        <div class="d-flex">
+                                            <img class="m-2" src="${item.img}">
+                                            <div>
+                                                <p class="m-2 text-bold">${item.name}</p>
+                                                <p class="m-2">${item.role}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> <!-- end of swiper-slide -->`
+        list.innerHTML += div
+
+        var cardSlider = new Swiper(".card-slider", {
+            autoplay: {
+                delay: 4000,
+                disableOnInteraction: false,
+            },
+            loop: true,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            slidesPerView: 3,
+            // direction: getDirection(),
+            spaceBetween: 70,
+            breakpoints: {
+                // when window is <= 767px
+                767: {
+                    slidesPerView: 1,
+                },
+                // when window is <= 991px
+                1024: {
+                    slidesPerView: 2,
+                    spaceBetween: 40,
+                },
+            },
+        });
+    }
+}
+
+function getListJoiMember() {
+    var list = document.getElementById('join')
+
+    for (let item of listJoinMember) {
+        var div = `<div style="margin-bottom: 0 !important;" class="members-join d-flex col-xxl-6 swiper-slide">
+                                <img class="avatar-member-join" src="${item.img}">
+                                <div>
+                                    <p class="name text-bold">${item.name}</p>
+                                    <p class="role">${item.role}</p>
+                                </div>
+                            </div>`
+        list.innerHTML += div
+    }
+    var cardSlider1 = new Swiper(".swiper-join-member", {
+        direction: 'vertical',
+        effect: 'slide',
+        slidesPerView: 3,
+        spaceBetween: 70,
+        loop: true,
+        autoplay: {
+            delay: 2000,
+            reverseDirection: true,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            1024: {
+                slidesPerView: 2,
+            },
+        },
+    })
+};
+
+function getListComment() {
+    var list = document.getElementById('comment')
+
+    for (let item of listComment) {
+        var div = `<div class="swiper-slide">
+                            <div class="container comment-card  testimonial-card pb-5">
+                                <div  class="member d-flex pt-4">
+                                    <div class="justify-content-lg-start">
+                                        <div class="m-3" >
+                                            <p>${item.line1}</p>
+                                        </div>
+
+                                        <div class="d-flex">
+                                            <div style="border-radius: 25%">
+                                                <img class="m-2" src="${item.img}">
+                                            </div>
+                                            <div>
+                                                <p class="m-2 text-bold">${item.name}</p>
+                                                <p class="m-2">${item.role}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> <!-- end of swiper-slide -->`
+        list.innerHTML += div
+    }
+    var cardSlider = new Swiper(".card-slider", {
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        slidesPerView: 3,
+        // direction: getDirection(),
+        spaceBetween: 70,
+        breakpoints: {
+            // when window is <= 767px
+            767: {
+                slidesPerView: 1,
+            },
+            // when window is <= 991px
+            1024: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+        },
+        
+    });
+};
