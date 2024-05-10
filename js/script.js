52d4c01ebc3b285b887bb5b0a61919798adb01f8
lucenguyen <83016168+lucenguyen@users.noreply.github.com>
2024-05-10 14:18:39 +0700

    edit background image, list join, and refactor code

--- js/script.js
+++ js/script.js
@@ -62,6 +62,24 @@ var cardSlider = new Swiper(".card-slider", {
     },
 });
 
+var cardSlider1 = new Swiper(".swiper-join-member", {
+    direction: 'vertical',
+    effect: 'slide',
+    slidesPerView: 3,
+    spaceBetween: 70,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        reverseDirection: true,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        1024: {
+            slidesPerView: 2,
+        },
+    },
+})
+
 /* BACK TO TOP BUTTON */
 // GET THE BUTTON
 myButton = document.getElementById("myBtn");
@@ -81,3 +99,91 @@ AOS.init({
     easing: "ease",
     once: true, // whether animation should happen only once - while scrolling down
 });
+
+const wrapElement = document.querySelector('#slider')
+
+setInterval(slidingText, 3000)
+
+function slidingText() {
+    wrapElement.classList.add('animated')
+    setTimeout(loopText, 500)
+}
+
+function loopText() {
+    const firstElement = document.querySelector('#slider div')
+    wrapElement.appendChild(firstElement)
+    wrapElement.classList.remove('animated')
+}
+
+
+let listOfOwners1 = [
+    {name1: "Lê Nguyễn Bảo Trâm", role1: "Chủ Hụi", image1: "assets/images/bao-tram.png", avatar: "assets/images/bao-tram-small.png"},
+    {name1: "Lê Huỳnh Chí Nhân", role1: "Chủ Hụi", image1: "assets/images/chi-nhan.png", avatar: "assets/images/chi-nhan-small.png"},
+    {name1: "Huỳnh Thất", role1: "Chủ Hụi", image1: "assets/images/huynh-that.png", avatar: "assets/images/huynh-that-small.png"},
+    {name1: "Văn Nguyễn Hoàng Lâm", role1: "Chủ Hụi", image1: "assets/images/hoang-lam.png", avatar: "assets/images/hoang-lam-small.png"},
+    {name1: "Lê Nguyễn Bảo Trâm", role1: "Chủ Hụi", image1: "assets/images/bao-tram.png", avatar: "assets/images/bao-tram-small.png"},
+    {name1: "Lê Huỳnh Chí Nhân", role1: "Chủ Hụi", image1: "assets/images/chi-nhan.png", avatar: "assets/images/chi-nhan-small.png"},
+    {name1: "Huỳnh Thất", role1: "Chủ Hụi", image1: "assets/images/huynh-that.png", avatar: "assets/images/huynh-that-small.png"},
+    {name1: "Văn Nguyễn Hoàng Lâm", role1: "Chủ Hụi", image1: "assets/images/hoang-lam.png", avatar: "assets/images/hoang-lam-small.png"},
+];
+
+let listSuggest = [
+    {line1: "Bắt đầu hoạt động từ: 24/02/2021", line2: "Chủ trương hội: Hoạt động năng nổ", line3: "Số lượng thành viên: 15", line4: "Mức độ đánh giá: Cực tốt", img: "assets/images/bao-tram-small.png", name: "Lê Nguyễn Bảo Trâm", role: "Chủ hụi"},
+    {line1: "Bắt đầu hoạt động từ: 24/02/2021", line2: "Chủ trương hội: Hoạt động năng nổ", line3: "Số lượng thành viên: 15", line4: "Mức độ đánh giá: Cực tốt", img: "assets/images/bao-tram-small.png", name: "Lê Nguyễn Bảo Trâm", role: "Chủ hụi"},
+    {line1: "Bắt đầu hoạt động từ: 24/02/2021", line2: "Chủ trương hội: Hoạt động năng nổ", line3: "Số lượng thành viên: 15", line4: "Mức độ đánh giá: Cực tốt", img: "assets/images/bao-tram-small.png", name: "Lê Nguyễn Bảo Trâm", role: "Chủ hụi"},
+    {line1: "Bắt đầu hoạt động từ: 24/02/2021", line2: "Chủ trương hội: Hoạt động năng nổ", line3: "Số lượng thành viên: 15", line4: "Mức độ đánh giá: Cực tốt", img: "assets/images/bao-tram-small.png", name: "Lê Nguyễn Bảo Trâm", role: "Chủ hụi"},
+];
+
+
+
+getListOwners(listOfOwners1);
+// getListSuggest(listSuggest);
+
+function getListOwners() {
+    var list = document.getElementById('owners')
+
+    for (let item of listOfOwners1) {
+        var div = `<div class="col-xxl-3 col-md-6 col-sm-12"><div class="member-chuhui justify-content-lg-start">
+                            <img  class="img-chu-hui" src="${item.image1}">
+                            <div class="d-flex">
+                                <img class="m-2" src="${item.avatar}">
+                                <div>
+                                    <p id="name" class="m-2 text-bold">${item.name1}</p>
+                                    <p id="role" class="m-2 roles">${item.role1}</p>
+                                </div>
+                            </div>
+                        </div></div>`
+        list.innerHTML += div
+    }
+}
+
+// function getListSuggest() {
+//     var list = document.getElementById('suggest')
+//
+//     for (let item of listSuggest) {
+//         var div = `<div class="swiper-slide">
+//                             <div class="container testimonial-card pb-5 ">
+//                                 <div  class="member d-flex pt-4 ">
+//                                     <div class="justify-content-lg-start">
+//                                         <div class="m-3" >
+//                                             <p>${item.line1}</p>
+//                                             <p>${item.line2}</p>
+//                                             <p>${item.line3}</p>
+//                                             <p class="mb-3">${item.line4}</p>
+//                                         </div>
+//
+//                                         <div class="d-flex">
+//                                             <img class="m-2" src="${item.img}">
+//                                             <div>
+//                                                 <p class="m-2 text-bold">${item.name}</p>
+//                                                 <p class="m-2">${item.role}</p>
+//                                             </div>
+//                                         </div>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         </div> <!-- end of swiper-slide -->`
+//         list.innerHTML += div
+//     }
+// }
+
